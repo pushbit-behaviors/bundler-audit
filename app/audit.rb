95d042d39f08134601eaa85824a291764de17aa4
Luke Roberts <email@luke-roberts.co.uk>
2016-01-25 20:45:21 -0800

    port audit code and remove git clone

---
+++ app/audit.rb
@@ -0,0 +1,20 @@
+lib_dir = File.expand_path(File.join(File.dirname(__FILE__),'bundler-audit','lib'))
+$LOAD_PATH << lib_dir unless $LOAD_PATH.include?(lib_dir)
+
+require 'ostruct'
+require 'json'
+
+require_relative './bundler-audit/lib/bundler/audit/advisory'
+require_relative './bundler-audit/lib/bundler/audit/scanner'
+require_relative './bundler-audit/lib/bundler/audit/version'
+
+
+def scan
+  results = []
+  Bundler::Audit::Scanner.new(File.expand_path "../code").scan(:ignore => []) do |result|
+    if result.class == Bundler::Audit::Scanner::UnpatchedGem
+      results << result
+    end
+  end
+  results
+end
